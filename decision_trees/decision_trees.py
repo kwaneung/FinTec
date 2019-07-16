b586c239f04edb42be9e8ecf7be8abe617b0b6c8
kwaneung <42456925+kwaneung@users.noreply.github.com>
2019-07-16 12:00:52 +0900

    .

--- decision_trees/decision_trees.py
+++ decision_trees/decision_trees.py
@@ -1,5 +1,3 @@
-import pandas as pd
-import glob
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn import tree
@@ -7,8 +5,20 @@ from sklearn.metrics import accuracy_score
 from sklearn.tree import export_graphviz
 from sklearn import datasets
 import pydotplus
+from linear_algebra import dot
+import random, math
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import numpy as np
+import glob
 import os
 os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+
+def logistic(x):
+    if x < 0:
+        return 1 - 1 / (1 + math.exp(x))
+    else:
+        return 1 / (1 + math.exp(-x))
 
 if __name__ == '__main__':
 
@@ -36,19 +46,23 @@ if __name__ == '__main__':
 
     feature = ["미국 내구재 주문", "미국 소비자 물가 상승률", "미국 소비율"]
     feature_cols = ["US Durable Goods Order", "US consumer price inflation", "US consumption rate"]
+    Dependent = 'cm5down'
     dfx = frame[feature]
-    dfy = frame[["cm5up"]]
+    dfy = frame[[Dependent]]
     # print(dfx.shape)
     # print(dfy.shape)
 
-    X_train, X_test, y_train, y_test = train_test_split(dfx, dfy, test_size=0.3, random_state=0)
+    # X_train, X_test, y_train, y_test = train_test_split(dfx, dfy, test_size=0.3)
+
+    # sc = StandardScaler()
+    # sc.fit(X_train)
+    X_test = frame[feature].iloc[292:, :]
+    X_train = frame[feature].iloc[:292, :]
+    y_train = frame[Dependent].iloc[:292]
+    y_test = frame[Dependent].iloc[292:]
 
     sc = StandardScaler()
     sc.fit(X_train)
-    X_test = frame[feature].iloc[292:, :]
-    X_train = frame[feature].iloc[:292, :]
-    y_train = frame['cm5up'].iloc[:292]
-    y_test = frame['cm5up'].iloc[292:]
 
     X_train_std = sc.transform(X_train)
     X_test_std = sc.transform(X_test)
@@ -60,8 +74,45 @@ if __name__ == '__main__':
     print('정확도 : %.2f' % accuracy_score(y_test, y_pred))
 
     iris = datasets.load_iris()
-    dot_data = export_graphviz(tree, out_file=None, feature_names=feature_cols, class_names=['0', '5% JUMP'], filled=True, rounded=True, special_characters=True)
+    dot_data = export_graphviz(tree, out_file=None, feature_names=feature_cols, class_names=['0', Dependent], filled=True, rounded=True, special_characters=True)
     graph = pydotplus.graph_from_dot_data(dot_data)
     # Image(graph.create_png())
     # (graph,) = pydot.graph_from_dot_file('DT.dot', encoding='utf8')
     graph.write_png('decision_trees.png')
+
+    print()
+    X_train = X_train.values
+    y_train = y_train.values
+    X_test = X_test.values
+    y_test = y_test.values
+
+    random.seed(0)
+
+    myreg = LinearRegression(False).fit(X_train, y_train)
+
+    true_positives = false_positives = true_negatives = false_negatives = 0
+    p_list = []
+
+    for x_i, y_i in zip(X_test, y_test):
+        predict = logistic(dot(myreg.coef_, x_i))
+        p_list.append(1 if predict >= 0.5 else 0)
+
+        if y_i == 1 and predict >= 0.5:  # TP: paid and we predict paid
+            true_positives += 1
+        elif y_i == 1:  # FN: paid and we predict unpaid
+            false_negatives += 1
+        elif predict >= 0.5:  # FP: unpaid and we predict paid
+            false_positives += 1
+        else:  # TN: unpaid and we predict unpaid
+            true_negatives += 1
+
+    print("true_positives : " + str(true_positives))  # 실제 True인 정답을 True라고 예측 (정답)
+    print("false_negatives : " + str(false_negatives))  # 실제 True인 정답을 False라고 예측 (오답)
+    print("false_positives : " + str(false_positives))  # 실제 False인 정답을 True라고 예측 (오답)
+    print("true_negatives : " + str(true_negatives))  # 실제 False인 정답을 False라고 예측 (정답)
+
+    precision = true_positives / (true_positives + false_positives)
+    recall = true_positives / (true_positives + false_negatives)
+
+    print("precision", precision)  # 정밀도. True라고 분류한 것 중에서 실제 True인 것의 비율
+    print("recall", recall)  # 재현율. 실제 True인 것 중에서 모델이 True라고 예측한 것의 비율
